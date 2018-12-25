e7699a94c19cd4dae5947003dce577ec817cdba9
Heads <heads@mail.pt>
2018-12-25 19:43:35 +0000

    Added mess.bat

---
+++ mess.bat
@@ -0,0 +1,254 @@
+@echo off
+pushd "%~dp0"
+set nvram=C:\Users\Heads\OneDrive\SAVEDG~1\Mess\nvram
+set romPath="roms;R:\Arcade\MAME Roms\Others;R:\Others\MAME Assets\MAME Devices;R:\Others\MAME Non Arcade Software;R:\Others\MAME Non Arcade System Clones"
+
+if [%1]==[gnw] goto :callMessWithDummyMachineArg
+if [%1]==[a2600] goto :a2600
+if [%1]==[a7800] goto :a7800
+if [%1]==[coleco] goto :coleco
+if [%1]==[odyssey2] goto :odyssey2
+if [%1]==[pce] goto :pce
+if [%1]==[famicom] goto :famicom
+if [%1]==[genesis] goto :genesis
+if [%1]==[gamegear] goto :gamegear
+if [%1]==[sms] goto :sms
+if [%1]==[plus4] goto :plus4
+if [%1]==[pico] goto :pico
+if [%1]==[vsmile] goto :vsmile
+if [%1]==[svision] goto :svision
+
+:callMess
+powershell -File .\mess.ps1 %* -skip_gameinfo -nowindow -rompath %romPath% -nvram_directory %nvram%
+goto :end
+
+:callMessWithDummyMachineArg
+set dir="%~dp2"
+set rom=%~n2
+powershell -File .\mess.ps1 %1 %rom% -skip_gameinfo -nowindow -rompath %romPath%;%dir% -nvram_directory %nvram%
+goto :end
+
+:a2600
+set rom="%~nx3"
+
+if not %rom:(PAL)=%==%rom% (
+	%0 a2600p %2 %3
+)
+
+goto :callMess
+
+:a7800
+set rom="%~nx3"
+
+if not %rom:(Europe)=%==%rom% (
+	%0 a7800p %2 %3
+)
+
+goto :callMess
+
+:coleco
+set rom="%~nx3"
+
+if not %rom:(Europe)=%==%rom% (
+	%0 colecop %2 %3
+)
+
+goto :callMess
+
+:odyssey2
+set rom="%~nx3"
+
+if not %rom:(Euro=%==%rom% (
+	if %rom:USA)=%==%rom% (
+		%0 videopac %2 %3
+	)
+)
+
+goto :callMess
+
+:pce
+7z l %3 | find "(japan)" >nul
+
+if %errorlevel%==1 (
+	%0 tg16 %2 %3
+)
+
+goto :callMess
+
+:famicom
+if [%5]==[] goto :callMess
+set rom=%~n5
+set romPath=%~dp5
+set romPath="%romPath:~0,-1%"
+call :callMess %1 %2 %3 %4 %rom%
+goto :end
+
+:genesis
+set rom="%~nx3"
+set specialPart=%rom:*(=%
+set specialPart=%specialPart:"=%
+
+if "%specialPart%"==%rom% goto :callMess
+set endSpecialPart=%specialPart:*)=%
+call set specialPart=%%specialPart:)%endSpecialPart%=%%
+
+if "%specialPart%"=="1" (
+	%0 megadrij %2 %3
+)
+
+if "%specialPart%"=="8" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="As" (
+	%0 megadrij %2 %3
+)
+
+if "%specialPart%"=="B" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="Ch" (
+	%0 megadrij %2 %3
+)
+
+if "%specialPart%"=="D" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="F" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="G" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="Gr" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="HK" (
+	%0 megadrij %2 %3
+)
+
+if "%specialPart%"=="I" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="K" (
+	%0 megadrij %2 %3
+)
+
+if "%specialPart%"=="Nl" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="No" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="S" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="Sw" (
+	%0 megadriv %2 %3
+)
+
+if "%specialPart%"=="UK" (
+	%0 megadriv %2 %3
+)
+
+set specialPartCheck=%specialPart:U=%
+if not "%specialPartCheck%"=="" set specialPartCheck=%specialPartCheck:J=%
+if not "%specialPartCheck%"=="" set specialPartCheck=%specialPartCheck:E=%
+
+if not "%specialPartCheck%"=="" goto :callMess
+
+if "%specialPart:U=%"=="%specialPart%" (
+	if "%specialPart:E=%"=="%specialPart%" (
+		%0 megadrij %2 %3
+	)
+		
+	%0 megadriv %2 %3
+)
+
+goto :callMess
+
+:gamegear
+set rom="%~nx3"
+
+if not %rom:(Jpn)=%==%rom% (
+	%0 gamegeaj %2 %3
+)
+
+goto :callMess
+
+:sms
+set rom="%~nx3"
+
+if not %rom:(Kor)=%==%rom% (
+	%0 smskr %2 %3
+)
+
+if not %rom:(Bra)=%==%rom% (
+	%0 smspal %2 %3
+)
+
+if not %rom:(Euro=%==%rom% (
+	%0 smspal %2 %3
+)
+
+if not %rom:(Jpn)=%==%rom% (
+	%0 smsj %2 %3
+)
+
+goto :callMess
+
+:plus4
+set rom="%~nx3"
+
+if not %rom:(PAL)=%==%rom% (
+	%0 plus4p %2 %3
+)
+
+goto :callMess
+
+:pico
+set rom="%~nx3"
+
+if not %rom:(USA=%==%rom% (
+	%0 picou %2 %3
+)
+
+if not %rom:(Jpn=%==%rom% (
+	%0 picoj %2 %3
+)
+
+goto :callMess
+
+:vsmile
+set rom="%~nx3"
+
+if not %rom:(Ger=%==%rom% (
+	%0 vsmileg %2 %3
+)
+
+if %rom:(USA=%==%rom% (
+	%0 vsmilef %2 %3
+)
+
+goto :callMess
+
+:svision
+set rom="%~nx3"
+
+if not %rom:(Euro=%==%rom% (
+	%0 svisionp %2 %3
+)
+
+%0 svisionn %2 %3
+
+:end
+popd
